e5f75743f58979d7937520d58e7109a190d3015d
Rebecca Medina (RIT Student) <rcm3517@ad.rit.edu>
2019-03-07 17:10:23 -0500

    Add more interaction and second graph

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # 460_proj1
+
+## Dataset description
+
+The data set I chose is information about the number of deaths due to the top ten leading causes of death from the years 1999 to 2016. This data was found
+at https://catalog.data.gov/dataset/age-adjusted-death-rates-for-the-top-10-leading-causes-of-death-united-states-2013 .
+
+I opened the dataset in openrefine, but it was very clean. The numbers seemed reasonable overall, and there were no mispellings. I ended up not doing anything
+to the data. There are two columns that I did consider removing so that the data was more consice and did not use any excess memory. These were a more detailed 
+name for the cause of death, and a column named age-adjusted death rate. I did not clean these initially as I was not sure if I wanted to include these columns in the 
+final product. I needed to find out what the age-adjusted death rate was, but I could not understand from the description given on the dataset website. In the end, I did
+not use either of these columns. If I were to do this again, I would remove those columns of data. 
+
+## Decisions on Visualization
+
+Originally, I was thinking about using a bar chart. The user would be able to pick between both the states and the causes of death. However, I felt like the user would benefit from seeing the comparison between the different causes within a state. I then decided to change the graph to a line chart, where all the causes of death were their own line and the user could change between the states. When I completed this, I realized that the scale was too large for the differences and trends in the causes with fewer deaths. To combat this, I first got rid of one "cause" which was all causes combined. This made the scale change significantly, yet I still did not think this was enough. I decided to add back my original bar chart, but as an extra feature. If the user wished to take a closer look at a particular cause of death, they can click on the line and a new graph would be displayed with only that cause being shown. To be consistent, I made the bars the same color as the one used in the line chart.  
+
+## Discoveries 
+
+I tried to add several different features in the graph than we covered in class. One of these was snapping to dots or lines when moving the mouse. I ended up not incorporating this, but I found several interesting examples regarding this.
+
+## Challenges
+
+It was difficult to decide how to display the information so that the story was the fullest. Originally, there was a "all causes" row, which I ommited as it would skew the scale for the smaller of the causes of death. In the end, the top two causes had significantly higher numbers than the other causes, leading them to be crammed at the bottom of the graph. I also decided to use two graphs instead as I didn't think trends in one cause alone could be easily seen in the line chart. I do think this helped when analyzing the data.
+
+Another challenge was the colors. Since I decided to display all causes at the same time, I needed to use 10 colors, though it is recommended not to use more than five or seven colors. In the end, I actually used 11 colors, as I also had a hover over color. I felt it was more important to have a unique color for this than to try and get rid of colors. Instead, I greyed out all other data which was not being hovered over, so the line in question would pop out. I also made the stroke width smaller for the lines not being hovered over. 
+
+One thing which I could not solve, was a strange problem I only ran into under certain circumstances. Sometimes, the bar chart will not update colors of the rectangles. I could not find a connection or pattern for these bars, however. 
+
+## Resources
+
+I used the following resources: 
+
+https://stackoverflow.com/questions/6623231/remove-all-white-spaces-from-text
+http://bl.ocks.org/WilliamQLiu/bd12f73d0b79d70bfbae
+http://bl.ocks.org/d3noob/38744a17f9c0141bcd04
+http://bl.ocks.org/d3noob/a22c42db65eb00d4e369
+https://bl.ocks.org/pstuffa/26363646c478b2028d36e7274cedefa6
+https://bl.ocks.org/alandunning/cfb7dcd7951826b9eacd54f0647f48d3
+https://bl.ocks.org/d3noob/23e42c8f67210ac6c678db2cd07a747e
+http://www.d3noob.org/2014/07/my-favourite-tooltip-method-for-line.html
+https://bl.ocks.org/jkeohan/b8a3a9510036e40d3a4e
+Color Scheme: Office Room 2 from https://digitalsynopsis.com/design/beautiful-color-palettes-combinations-schemes/
--- main.js
+++ main.js
@@ -1,108 +1,209 @@
+//variables
+
 let dataset;
-let w=600;
-let h=500;
-let svg;
+const w=700;
+const h=500;
+let svg, svg2, exitButton;
 let xScale, yScale;
+let xScale2, yScale2;
 let xAxis, yAxis;
+let xAxis2, yAxis2;
 let xAxisGroup, yAxisGroup;
+let xAxisGroup2, yAxisGroup2;
 let state = "Alabama";
 let cause = "Cancer";
 let maxDeath;
-
-let dataUrl = "NCHS_-_Leading_Causes_of_Death__United_States.csv";
-
+const dataUrl = "NCHS_-_Leading_Causes_of_Death__United_States.csv";
 let groupByState;
-
-let keyFunction = (d) => `${d.cause}${d.year}`;
-
+let lineMatrix = {};
 
 let line = d3.line()
     .x((d) => {return xScale(d.year)})
-    .y((d) => {return yScale(d.deaths)})
-    //.curve(d3.curveMonotoneX)
+    .y((d) => {return yScale(d.deaths)});
 
-
-let lineMatrix = {}
 
 let colorMatrix = {
-    "Unintentional injuries": "red",
+    "Unintentional injuries": "teal",
     "Alzheimer's disease": "orange",
-    "Cancer": "yellow",
+    "Cancer": "brown",
     "CLRD": "green",
     "Diabetes": "blue",
     "Heart disease": "purple",
     "Influenza and pneumonia": "black",
-    "Kidney disease": "brown",
+    "Kidney disease": "yellow",
     "Stroke": "pink",
-    "Suicide": "lavendar"
-}
+    "Suicide": "magenta"
+};
 
 function rowConverter(d) {
     return {
-        year:  Date.parse(+d.Year),
+        year:  d3.timeParse('%Y')(+d.Year),
         cause: d["Cause Name"],
         state: d.State,
         deaths: parseInt(d.Deaths)
     }
 }
 
-function handleMouseOver(d, i){  
+//mouse over the circles or over bars in the second graph
+function handleMouseOver(d, i){ 
+    //update and reveal extra div 
     d3.select('#barInfo')
       .classed('hidden', false)
-      .style('margin-left', xScale(d.year) + 'px')
-      .style('margin-top',  yScale(d.deaths) -70+ 'px')
-      .html(`<strong>Year:</strong> ${d3.timeFormat('%Y')(d.year)}<br/> <strong>Deaths:</strong> ${d.deaths}`);
-      
-    d3.select(this).style('fill', 'green');
+      .style('margin-left', d3.event.pageX + 'px')
+      .style('margin-top',  d3.event.pageY -140+ 'px')
+      .html(`<strong>Cause:</strong> ${d.cause}</br><strong>Year:</strong> ${d3.timeFormat('%Y')(d.year)}<br/> <strong>Deaths:</strong> ${d.deaths}`);
+    
+    //change color for extra pop
+    d3.select(this).style('fill', 'red');
 
-    console.log(d.year)
 }
 
+//mouse moves out of the circles or bars in second graph
 function handleMouseOut(d,i) {
+    //hide extra div
     d3.select('#barInfo').classed('hidden', true);
-    d3.select(this).style('fill', (d) => cScale(d.deaths));
+    
+    //reset color
+    d3.select(this).style('fill', (d) => colorMatrix[d.cause]);
 }
 
-function handleMouseClick(d,i) {
-    d3.select('.toggleOn').classed('toggleOn', false);
-    d3.select(this).classed('toggleOn', true);
-}
-
+//mouse clicks on a line
 function handleMouseClickLine(d){
 
+    //check if the second graph already exists
+   if(!svg2){ 
+       //if not, create a button to hide the graph
+       exitButton = document.createElement('button');
+       exitButton.innerText = "Hide Second Graph";
+       document.querySelector('#main2').appendChild(exitButton);
+       
+       //create the graph
+       svg2 = d3.select('#main2').append('svg').attr('width', w).attr('height', h);
+       
+       //event listener to hide the graph and button
+       exitButton.addEventListener('click', (e) => {
+           document.querySelectorAll('svg')[1].classList.add('hidden')
+           document.querySelector('button').classList.add('hidden');
+       })
+
+        //scales
+        yScale2 = d3.scaleLinear()
+            .domain([0,d3.max(groupByState[state][d[0].cause], (d)=> d.deaths)])
+            .range([h-50, 20]);
+
+
+        xScale2 = d3.scaleTime()
+            .domain([d3.min(groupByState[state][d[0].cause], (d)=> d.year), d3.timeYear.offset(d3.max(groupByState[state][d[0].cause], (d)=> d.year),1)])
+            .range([70, w-40]);
+
+
+        //bars
+        svg2.selectAll('.bars')
+            .data(groupByState[state][d[0].cause])
+            .enter()
+            .append('rect')
+            .classed('bars', true)
+            .attr('x', (d) => xScale2(d.year))
+            .attr('y', (d) => yScale2(d.deaths))
+            .attr('height', (d) =>  h-50-yScale2(d.deaths))
+            .attr('width', 20)
+            .attr('fill', (d) => colorMatrix[d.cause])
+            .on('mouseover', handleMouseOver)
+            .on('mouseout', handleMouseOut);
+
+        
+        //axis
+        //could actually re use this...
+        xAxis2 = d3.axisBottom(xScale2)
+            .ticks(17)
+            .tickFormat(d3.timeFormat('%Y'));
+        xAxisGroup2 = svg2.append('g')
+             .attr('transform', `translate(0, ${h - 50})`)
+             .call(xAxis2);
+        //text label for x axis
+        svg2.append("text")             
+             .attr("transform",`translate(${w/2}, ${h-10})`)
+             .style("text-anchor", "middle")
+             .text("Year");
+
+        yAxis2 = d3.axisLeft(yScale2)
+            .tickFormat(d3.format(".3s"));
+        yAxisGroup2 = svg2.append('g')
+             .attr('transform', `translate(70, 0)`)
+             .call(yAxis2);
+        // text label for y axis
+        svg2.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 5)
+            .attr("x",0 - (h / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("Number of Deaths");  
+    }else {
+        //un-hide the graph if hidden
+        document.querySelectorAll('svg')[1].classList.remove('hidden')
+        document.querySelector('button').classList.remove('hidden');
+        
+        //update y scale
+        yScale2.domain([0,d3.max(groupByState[state][d[0].cause], (d)=> d.deaths)]);
+        yAxisGroup2.transition()
+            .duration(1000)
+            .call(yAxis2);
+
+        //update bars
+        svg2.selectAll(`.bars`)
+            .data(groupByState[state][d[0].cause])
+            .transition()
+            .duration(1000)
+            .attr('y', (d) => yScale2(d.deaths))
+            .attr('height', (d) =>  h-50-yScale2(d.deaths))
+            .attr('fill', (d) => colorMatrix[d.cause])
+    }
+
 }
 
+//for mouse over the lines
 function handleMouseOverLine(d){
-    console.log(d)
+    //update and show extra div
     d3.select('#barInfo')
     .classed('hidden', false)
-    .style('margin-left', xScale(d.year) + 'px')
-    .style('margin-top',  yScale(d.deaths) -70+ 'px')
-    .html(`<strong>Cause:</strong> ${d[0].cause}<br/> <strong>Deaths:</strong> ${d.deaths}`);
+    .style('margin-left', d3.event.pageX + 'px')
+    .style('margin-top',  d3.event.pageY -140+ 'px')
+    .html(`<strong>Cause:</strong> ${d[0].cause}<br/>`);
     
-
-    d3.selectAll(".line").style('stroke', (d) => "grey");
-    d3.select(this).style('stroke', (d) => "red");
+    //grey everything out
+    d3.selectAll(".line").style('stroke', (d) => "grey").style('stroke-width', 2);
+    d3.selectAll(".dot").style('fill', (d) => "grey");
+    
+    //color selected line and respective circles red
+    d3.select(this).style('stroke', (d) => "red").style('stroke-width', 3);
+    d3.selectAll(`.dot-${d[0].cause.replace(/ /g,'').replace('\'','')}`).style('fill', (d) => "red");
 }
 
+//for mouse moving out of line
 function handleMouseOutLine(d) {
-    d3.selectAll(".line").transition().duration(500).style('stroke', (d) => colorMatrix[d[0].cause]);
+    //set line colors to normal
+    d3.selectAll(".line").transition().duration(500).style('stroke', (d) => colorMatrix[d[0].cause]).style('stroke-width', 3);
+    //set circle colors to normal
+    d3.selectAll(".dot").transition().duration(500).style('fill', (d) => colorMatrix[d.cause]);
+   //hide the extra div
     d3.select('#barInfo').classed('hidden', true);
 }
+
 
 function initGraph() {
 
-        svg = d3.select('#main').append('svg').attr('width', w).attr('height', h);
+        svg = d3.select('#main2').append('svg').attr('width', w).attr('height', h);
 
         //scales
         yScale = d3.scaleLinear()
             .domain([0,maxDeath[state]])
-            .range([h-20, 20]);
+            .range([h-50, 20]);
         
             
         xScale = d3.scaleTime()
             .domain([d3.min(groupByState[state][cause], (d)=> d.year), d3.max(groupByState[state][cause], (d)=> d.year)])
-            .range([40, w-40]);
+            .range([70, w-20]);
 
 
         //AXIS
@@ -110,20 +211,30 @@ function initGraph() {
             .ticks(17)
             .tickFormat(d3.timeFormat('%Y'));
         xAxisGroup = svg.append('g')
-             .attr('transform', `translate(0, ${h - 20})`)
+             .attr('transform', `translate(0, ${h - 50})`)
              .call(xAxis);
+        svg.append("text")             
+             .attr("transform",`translate(${w/2}, ${h-10})`)
+             .style("text-anchor", "middle")
+             .text("Year");
 
-        yAxis = d3.axisLeft(yScale);
+        yAxis = d3.axisLeft(yScale)
+            .tickFormat(d3.format(".3s"));
         yAxisGroup = svg.append('g')
-             .attr('transform', `translate(40, 0)`)
+             .attr('transform', `translate(70, 0)`)
              .call(yAxis);
+        svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 5)
+            .attr("x",0 - (h / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("Number of Deaths");  
 
-
-             ///////LINE
+        //lines
         for(let key in groupByState[state]){
-
             lineMatrix[key] = svg.append('path')
-            .datum(groupByState[state][key], keyFunction)
+            .datum(groupByState[state][key])
             .attr("class", "line")
             .attr("d", line)
             .attr("id", key)
@@ -131,77 +242,56 @@ function initGraph() {
             .on('mouseover', handleMouseOverLine)
             .on('mouseout', handleMouseOutLine)
             .on('click', handleMouseClickLine);
-
         }
 
+        //circles on the lines
         let i =0;
         for(let key in groupByState[state]){
         svg.selectAll(`.dot-${i}`)
-            .data(groupByState[state][key], keyFunction)
+            .data(groupByState[state][key])
             .enter().append("circle") // Uses the enter().append() method
-            .attr("class", "dot") // Assign a class for styling
+            .classed(`dot-${key.replace(/ /g,'').replace('\'','')}`, true)
+            .classed('dot', true)
             .attr("cx", function(d, i) { return xScale(d.year) })
             .attr("cy", function(d) { return yScale(d.deaths) })
-            .attr("r", 3)
+            .attr("r", 4)
             .style("fill", colorMatrix[key])
+            .on('mouseover', handleMouseOver)
+            .on('mouseout', handleMouseOut);
             i++;
         }
 }
 
 function updateGraph() {
-
-
+    //update scale
     yScale.domain([0, maxDeath[state]]);
    
     yAxisGroup.transition()
-    .duration(1000)
-    .call(yAxis);
-
-//////////////LINE TRANSITION//////////////////
-    
-
-for(let key in groupByState[state]){
-    if(key==="All causes") continue;
-    lineMatrix[key].datum(groupByState[state][key])
-    .transition().duration(750)
-    .attr("d", line)
-
- }
-
-
-
- let i =0;
- for(let key in groupByState[state]){
-    svg.selectAll(`.dot-${i}`)
-        .data(groupByState[state][key], keyFunction)
-        .enter()
-        .append('circle')
-        .attr("class", "dot") // Assign a class for styling
-        .attr("cx", function(d, i) { return xScale(d.year) })
-        .attr("cy", function(d) { return yScale(d.deaths) })
-        .attr("r", 3)
-        .style("fill", colorMatrix[key])
-        .merge(svg.selectAll(`.dot-${i}`))
-        .transition()
-        .duration(500)
-        .attr("cx", function(d, i) { return xScale(d.year) })
-        .attr("cy", function(d) { return yScale(d.deaths) })
-        .style("fill", colorMatrix[key])
-        i++;
-
-
-
-        
-    svg.selectAll(`.dot-${i}`)
-    .data(groupByState[state][key], keyFunction).exit()
-        .transition()
         .duration(1000)
-        .remove();
- }
+        .call(yAxis);
+    
+    //update lines
+    for(let key in groupByState[state]){
+        if(key==="All causes") continue;
+        lineMatrix[key].datum(groupByState[state][key])
+        .transition().duration(750)
+        .attr("d", line)
+
+    }
+
+    //update circles
+    let i =0;
+    for(let key in groupByState[state]){
+        svg.selectAll(`.dot-${key.replace(/ /g,'').replace('\'','')}`)
+            .data(groupByState[state][key])
+            .transition()
+            .duration(750)
+            .attr("cy", function(d) { return yScale(d.deaths) })
+            i++;
+    }
 }
 
 window.onload = function() {
-    let causeDropDown = document.querySelector('#cause');
     let stateDropDown = document.querySelector('#state');
 
     d3.csv(dataUrl, rowConverter).then((data) => {
@@ -213,6 +303,7 @@ window.onload = function() {
             .key(function(d) {return d.cause})
             .object(dataset);
 
+        //fill the states selector
         for(let key in groupByState){
             let newSelect = document.createElement('option');
             newSelect.value = key;
@@ -221,26 +312,43 @@ window.onload = function() {
             delete groupByState[key]["All causes"]
         }
 
-        for(let key in groupByState["Alabama"]){
-            let newSelect = document.createElement('option');
-            newSelect.value = key;
-            newSelect.innerText = key;
-            causeDropDown.appendChild(newSelect);
-        }
-
+        //find info on max number of deaths per state
         maxDeath = d3.nest()
-        .key((d) => d.state)
-        .rollup((v) => d3.max(v, (d) => {
-            if(d.cause!=="All causes")return d.deaths;
-            return 0;
-        }))
-        .object(dataset)
+            .key((d) => d.state)
+            .rollup((v) => d3.max(v, (d) => {
+                if(d.cause!=="All causes")return d.deaths;
+                return 0;
+            }))
+            .object(dataset)
         
         initGraph();
 
+        //make the legend
+        let main = document.querySelector('#main2');
+        let container = document.createElement('div');
+        for(let key in groupByState["Alabama"]){
+            let newSpan = document.createElement('span');
+            let newLedgeSpan = document.createElement('span');
+            let newLedgeText = document.createElement('span');
+
+            newLedgeSpan.classList.add('legend-rect');
+            newLedgeSpan.style.backgroundColor = colorMatrix[key];
+            newSpan.appendChild(newLedgeSpan)
+
+            newLedgeText.innerText = `${key}`;
+            newLedgeSpan.style.marginLeft = '5px';
+            newSpan.appendChild(newLedgeText)
+
+            newSpan.style.marginLeft = '20px'
+
+            container.appendChild(newSpan);
+        }
+        
+        container.style.display = 'block';
+        container.style.margin = 'auto';
+        main.appendChild(container);
     })
 
     //add event listeners
-    causeDropDown.addEventListener("change", (e) => {cause = e.target.value; updateGraph()})
     stateDropDown.addEventListener("change", (e) => {state = e.target.value; updateGraph()})
 }
